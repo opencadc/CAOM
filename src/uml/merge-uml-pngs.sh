3c6df65f3bb9a0f36e243087f94305055116818e
Patrick Dowler <pdowler.cadc@gmail.com>
2024-08-07 14:10:59 -0700

    allow multiple Plane.energy.bandpassName redefine MultiPolygon in DALI-1.2 style; add direct reference in Plane.position

--- src/uml/merge-uml-pngs.sh
+++ src/uml/merge-uml-pngs.sh
@@ -2,9 +2,9 @@
 
 ## complete list
 #convert -append CAOM1core.png CAOM2datatypes.png CAOM3wcs.png CAOM4entities.png CAOM5vocabularies.png \
-	draft-CAOM-2.5.png
+#	draft-CAOM-2.5.png
 
 ## only changes
-convert -append CAOM1core.png CAOM4entities.png CAOM5vocabularies.png \
-        draft-CAOM-2.5.png
+magick CAOM1core.png CAOM2datatypes.png CAOM4entities.png CAOM5vocabularies.png \
+        -append draft-CAOM-2.5.png
 
