6565b1734cc0a6c917f65b87c4bd01414f992019
Patrick Dowler <pdowler.cadc@gmail.com>
2024-08-15 10:46:11 -0700

    fix UML for Entity refactoring done in VODML

--- src/uml/merge-uml-pngs.sh
+++ src/uml/merge-uml-pngs.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-## complete list
-#convert -append CAOM1core.png CAOM2datatypes.png CAOM3wcs.png CAOM4entities.png CAOM5vocabularies.png \
-#	draft-CAOM-2.5.png
-
 ## only changes
+pushd src/main/resources
 magick CAOM1core.png CAOM2datatypes.png CAOM4entities.png CAOM5vocabularies.png \
         -append draft-CAOM-2.5.png
+popd
 
