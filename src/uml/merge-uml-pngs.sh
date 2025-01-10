831a1a3940c13d1990aeb5ad67ad843fb96bb02e
Patrick Dowler <pdowler.cadc@gmail.com>
2025-01-09 17:39:03 -0800

    regen draft UML for doc

--- src/uml/merge-uml-pngs.sh
+++ src/uml/merge-uml-pngs.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ## only changes
-pushd src/main/resources
-magick CAOM1core.png CAOM2datatypes.png CAOM4entities.png CAOM5vocabularies.png \
-        -append draft-CAOM-2.5.png
+
+pushd src/uml
+
+magick CAOM1core.png CAOM2datatypes.png CAOM3vocabularies.png CAOM4entities.png \
+        -append ../main/resources/draft-CAOM-2.5.png
 popd
 
